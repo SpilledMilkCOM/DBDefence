f54061346d0a3255e06c1d34f24fbcb4cb6334ee
Parker Smart <parker.smart@hexagon.com>
2021-04-21 18:19:16 -0500

    update README

--- src/SQL/TestDB.02.Populate_Contact_Table.sql
+++ src/SQL/TestDB.02.Populate_Contact_Table.sql
@@ -1,9 +1,9 @@
 USE TestDB
 
-INSERT INTO dbo.Contact ([FirstName],[LastName],[Password],[Email],[Phone]) VALUES ('Parker','Smart','ThisShouldBeEncrypted','parker.smart@hexagon.com','3033306139')
+INSERT INTO dbo.Contact ([FirstName],[LastName],[Password],[Email],[Phone]) VALUES ('Parker','Smart','ThisShouldBeEncrypted','parker.smart@gmail.com','3035551211')
 INSERT INTO dbo.Contact ([FirstName],[LastName],[Password],[Email],[Phone]) VALUES ('John','Doe','password','john.doe@gmail.com','3035551212')
 
 USE TestDBEncrypted
 
-INSERT INTO dbo.Contact ([FirstName],[LastName],[Password],[Email],[Phone]) VALUES ('Bruce','Wayne','adseife13$#@1zxhgr','bruce.wayne@batcave.com','3033306139')
+INSERT INTO dbo.Contact ([FirstName],[LastName],[Password],[Email],[Phone]) VALUES ('Bruce','Wayne','adseife13$#@1zxhgr','bruce.wayne@gmail.com','3035551211')
 INSERT INTO dbo.Contact ([FirstName],[LastName],[Password],[Email],[Phone]) VALUES ('The','Joker','hahahahahaha','joker@gmail.com','3035551212')
