e0c862f221b7968ec5d3d0e73ad3ca31894eceb3
Parker Smart <parker.smart@hexagon.com>
2021-04-22 22:37:32 -0500

    Free loaded library

--- src/DBDefence.ConsoleCpp/DbDefenceInit.cpp
+++ src/DBDefence.ConsoleCpp/DbDefenceInit.cpp
@@ -1,38 +1,40 @@
 #include <Windows.h>
 
 typedef void (WINAPI* INITPROC)(BOOL);
-typedef void (WINAPI* ADDPASSWORDPROC)(const wchar_t* /*Database*/, const wchar_t*/*Password*/);
+typedef void (WINAPI* ADDPASSWORDPROC)(const wchar_t* /*Database Name*/, const wchar_t*/*Password*/);
 
 bool InitializeDBDefence(const wchar_t* databaseName, const wchar_t* password) {
     bool result = false;
+    BOOL skipDialog = TRUE;
 
     // Get a handle to the DLL module.
 
-    HINSTANCE hinstLib = LoadLibrary(LR"(C:\tmp\DbDefence\dbd_clnt.dll)");
+    HINSTANCE dllInstanceHandle = LoadLibrary(LR"(C:\tmp\DbDefence\dbd_clnt.dll)");
 
     // If the handle is valid, try to get the function address.
 
-    if (hinstLib != NULL) {
+    if (dllInstanceHandle != NULL) {
 #pragma warning( push )
 #pragma warning(disable : 4191)
-        INITPROC InitializeProc = reinterpret_cast<INITPROC>(GetProcAddress(hinstLib, "Initialize"));
+        INITPROC InitializeProc = reinterpret_cast<INITPROC>(GetProcAddress(dllInstanceHandle, "Initialize"));
 
         // If the function address is valid, call the function.
 
         if (NULL != InitializeProc) {
-            (InitializeProc)(TRUE /*Skip dialog*/);
+            InitializeProc(skipDialog);
         }
 
         // Free the DLL module.
 
-        ADDPASSWORDPROC AddPwdAdd = reinterpret_cast<ADDPASSWORDPROC>(GetProcAddress(hinstLib, "AddPassword"));
+        ADDPASSWORDPROC AddPwdAdd = reinterpret_cast<ADDPASSWORDPROC>(GetProcAddress(dllInstanceHandle, "AddPassword"));
 
         if (NULL != AddPwdAdd) {
             AddPwdAdd(databaseName, password);
             result = true;
         }
 #pragma warning( pop )
-        // fFreeResult = FreeLibrary(hinstLib);
+
+        result &= FreeLibrary(dllInstanceHandle);
     }
 
     return result;
